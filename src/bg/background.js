5f545bb29addbd72b218ca7067dd4ad0c93f8a62
omgwtfkrieg <omgwtfkrieg@gmail.com>
2014-07-21 08:12:08 -0400

    Now injects punches to the TT website
    At this moment the app is able to simulate button and inject Badge ID
    into the TimeTrak web app.

---
+++ src/bg/background.js
@@ -0,0 +1,31 @@
+// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
+
+// var settings = new Store("settings", {
+//     "sample_setting": "This is how you use Store.js to remember values"
+// });
+
+
+//example of using a message handler from the inject scripts
+chrome.extension.onMessage.addListener(
+  function(request, sender, sendResponse) {
+  	chrome.pageAction.show(sender.tab.id);
+    sendResponse();
+
+  });
+  
+// Show page action icon in omnibar.
+function onWebNav(details) {
+    if (details.frameId === 0) {
+        // Top-level frame
+        chrome.pageAction.show(details.tabId);
+    }
+}
+var filter = {
+    url: [{
+        hostEquals: 'http://km2.timetrak.com:85/'
+    }]
+};
+chrome.webNavigation.onCommitted.addListener(onWebNav, filter);
+chrome.webNavigation.onHistoryStateUpdated.addListener(onWebNav, filter);
+
+
---
+++ src/inject/inject.js
@@ -0,0 +1,54 @@
+chrome.extension.sendMessage({}, function(response) {
+	var readyStateCheckInterval = setInterval(function() {
+	if (document.readyState === "complete") {
+		clearInterval(readyStateCheckInterval);
+
+		// ----------------------------------------------------------
+		// This part of the script triggers when page is done loading
+		//console.log("Hello. This message was sent from scripts/inject.js");
+		// ----------------------------------------------------------
+
+	}
+	}, 10);
+});
+
+chrome.storage.local.get('whatdo', function (result) { //***START***//
+	console.log('Current whatdo: ' + result.whatdo + ' value');
+	//Fires Event: Submits "In for the Day" with a button click.
+	if (result.whatdo == undefined ){
+		chrome.storage.local.set({'whatdo': "exefun1"}, function() {
+		console.log('Changed whatdo: undefined to whatdo: ' + result.whatdo);
+		});
+		
+	};			
+				
+	if (result.whatdo == "exefun1"){
+		chrome.storage.local.set({'whatdo': "exefun2"});
+		console.log('Changed whatdo: exefun1 to whatdo: ' + result.whatdo);
+		$("input[name=FN1]").click();
+		console.log('In for the Day submitted.');
+	};
+					
+	if (result.whatdo == "exefun2"){
+		console.log('Inside exefun2 function');
+		chrome.storage.local.set({'whatdo': "exefun1"});
+		console.log('Changed whatdo: exefun2 to whatdo: ' + result.whatdo);
+		$("input[name=MSG4]").val('41614');
+		if($("input[name=MSG4]").length && $("input[name=MSG4]").val().length){
+			console.log('Badge ID inserted');
+			$("input[name=OK]").click();
+		};
+		//$("input[name=OK]").click();
+		whatdo = null;
+		 return false;
+	};
+					
+}); //***END***//
+
+/* chrome.storage.local.get('color', function (result) {
+console.log('Changed whatdo: exefun2 to whatdo: ' + result.color);
+}); */
+
+chrome.storage.sync.get('color', function(items) {
+	console.log('color: ' + items.color);
+  });
