a4275a7717604b72c00f0ec096db745eacc9343c
omgwtfkrieg <omgwtfkrieg@gmail.com>
2014-07-28 21:31:37 -0400

    added bootstrap to the option page

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -64,3 +64,4 @@ var badgeID_CS;
 	};
 					
 }); //***END***//
+
--- src/options_custom/option_page.js
+++ src/options_custom/option_page.js
@@ -1,4 +1,4 @@
-// Saves options to chrome.storage
+/* // Saves options to chrome.storage
 function save_options() {
   var punchtype_OP = document.getElementById('punchtype_OP').value;
   var likesColor = document.getElementById('like').checked;
@@ -33,4 +33,55 @@ function restore_options() {
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
+document.getElementById('save').addEventListener('click', save_options); */
+
+ $(document).ready(function(){
+	starttimepicker ();
+	//getpunchval ();
+      var i=1;
+	  
+	 
+    $("#add_row").click(function(){
+		$('#addr'+i).html("<td>"+ (i+1) +"</td><td class='col-lg-4'><input type='text' name='badgeID_OP"+i+"'  placeholder='Badge ID: 12345' class='form-control' maxlength='5'/></td><td><div class='input-group bootstrap-timepicker'><input name='schedule"+i+"' class='form-control col-lg-2 timepicker1' type='text' ><span class='input-group-addon'><i class='glyphicon glyphicon-time'></i></span></div></td><td class='col-lg-4'><select name='punch_select"+i+"' class='form-control punch_type_OP'><option value='FN1'>In for the day</option><option value='FN2'>Back from lunch</option><option value='FN3'>Out</option><option value='FN5'>End of the day</option></select></td></tr>");
+		//starttimepicker ();
+		//getpunchval ();
+		$('.timepicker1').timepicker();
+		$('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
+		i++; 
+		
+	});
+	
+    $("#delete_row").click(function(){
+    	 if(i>1){
+		 $("#addr"+(i-1)).html('');
+		 i--;
+		 }
+		 getpunchval ();
+	});
+	
+
+	$('#tab_logic').find('input[type=text]').live('keyup', function(){
+		var key = $(this).attr('name');
+		localStorage[key] = $(this).val();
+	});
+	
+	for (var k in localStorage){
+		if (k.match(/^(query|url|value|punch)-(\d+)/)){
+			var type = RegExp.$1;
+			var id = RegExp.$2;
+			var c = createQuery(id);
+			c.find('.' + type).val(localStorage[k]);
+		}
+	};
+
+	$("select[name^='punch_select']").change(function () {
+		alert($(this).val())
+	});
+	
+
+  
+});
+
+function starttimepicker () {
+$('.timepicker1').timepicker();
+};
